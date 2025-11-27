37b69dcf87ef4f2f1c0d696ecade21b15fd71348
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2025-11-27 03:43:40 +0300

    config

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,21 @@
-import { redirect } from 'next/navigation';
 
+import HomePersonal from '@/components/HomePersonal';
+
+import generateStylesheetObject from '@/common/generateStylesheetsObject';
+
+export const metadata = {
+  title: 'Klinik - Modern Sağlık Hizmetleri',
+  icons: {
+    icon: '/assets/imgs/favicon.ico',
+    shortcut: '/assets/imgs/favicon.ico',
+    other: generateStylesheetObject([
+      '/assets/css/plugins.css',
+      '/assets/css/style.css',
+      'https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap',
+      'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@200;300;400;500;600;700&display=swap',
+    ]),
+  },
+};
 export default function Home() {
-  redirect('/home-personal');
+  return <HomePersonal />
 }
